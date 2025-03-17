acbbaf426b426e09a354abd6aee3be59950405f1
duong <22520298@gm.uit.edu.vn>
2025-03-17 09:25:49 +0700

    Layout Admin

---
+++ frontend-admin/src/app/components/sidebar/Sidebar.jsx
@@ -0,0 +1,79 @@
+"use client"
+import { Button } from "@/components/ui/button";
+import useSidebarStore from "@/store/sidebarStore";
+
+import { useRouter, usePathname } from "next/navigation";
+
+const SidebarItem = ({ path, icon, label }) => {
+    const router = useRouter();
+    const pathname = usePathname();
+    const { isSidebarOpen, toggleSidebar } = useSidebarStore();
+
+    const isActive = pathname === path;
+
+    const handleClick = () => {
+        router.push(path);
+        if (isSidebarOpen) {
+            toggleSidebar();
+        }
+    };
+
+    return (
+        <Button
+            variant="ghost"
+            className={`w-full justify-start mb-3 cursor-pointer flex items-center text-[15px] ${isActive ? "text-black" : "text-[#A3AED0]"
+                }`}
+            onClick={handleClick}
+        >
+            <img
+                src={icon}
+                alt={label}
+                className={`mr-3 w-5 h-6 ${isActive ? "filter brightness-0 invert-[10%]" : ""}`}
+            />
+            <span>{label}</span>
+        </Button>
+    );
+};
+
+const Sidebar = () => {
+    const { isSidebarOpen } = useSidebarStore();
+
+
+    const menuItems = [
+        { path: "/admin/dashboard", icon: "/svg/dashboard.svg", label: "Dashboard" },
+        { path: "/admin/users", icon: "/svg/user_admin.svg", label: "Người dùng" },
+        { path: "/admin/posts", icon: "/svg/book_admin.svg", label: "Sách" },
+        { path: "/admin/posts", icon: "/svg/return_admin.svg", label: "Mượn/Trả" },
+        { path: "/admin/documents", icon: "/svg/fine_admin.svg", label: "Phiếu phạt" },
+        { path: "/admin/supports", icon: "/svg/setting.svg", label: "Hỗ trợ" },
+    ];
+
+    return (
+        <aside
+            className={`fixed left-0 h-full w-52 p-4 transform transition-transform duration-200 ease-in-out md:translate-x-0 flex flex-col z-50 md:z-0 ${isSidebarOpen ? "translate-x-0 bg-white shadow-lg " : "-translate-x-full"
+                } ${isSidebarOpen ? "md:hidden" : ""} md:bg-white md:shadow-none`}
+        >
+            <div className="flex flex-col h-full overflow-y-auto">
+                {/* Logo */}
+                <div className="flex flex-col items-center mb-4">
+                    <img src="/images/logo.jpg" alt="Vibely Logo" className="w-36" />
+                </div>
+                <hr className="border-t border-gray-100 mb-6" />
+
+                {/* Navigation */}
+                <nav className="space-y-3 flex-grow">
+                    {menuItems.map((item) => (
+                        <SidebarItem key={item.path} {...item} />
+                    ))}
+                </nav>
+
+                {/* Footer Image */}
+                <div className="mt-auto flex flex-col items-center pb-4">
+                    <img src="/images/end_sidebar.png" alt="Vibely Logo" className="w-42" />
+                </div>
+            </div>
+        </aside>
+    );
+};
+
+export default Sidebar;
