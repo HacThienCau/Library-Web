ce6b887c95bd8ab0ed328a94243662a2554b492a
duong <22520298@gm.uit.edu.vn>
2025-04-05 19:26:14 +0700

    Backend MonoDB

---
+++ backend/library/src/main/java/com/library_web/library/Model/Sach.java
@@ -0,0 +1,23 @@
+package com.library_web.library.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Sach {
+    @Id
+    private Integer maSach;
+    private String tenSach;
+    private String moTa;
+    private String hinhAnh;
+    private Integer maTheLoai;
+    private Integer maTacGia;
+    private Integer soLuongTon;
+    private Integer soLuongMuon;
+}
--- backend/library/src/main/java/com/project/library/model/Book.java
+++
@@ -1,4 +0,0 @@
-package com.project.library.model;
-
-public class Book {
-}
