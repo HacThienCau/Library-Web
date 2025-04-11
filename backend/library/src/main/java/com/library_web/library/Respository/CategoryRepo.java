416bd9d4e1418aa1319fd15ebb607125b1785515
yurri2506 <22520590@gm.uit.edu.vn>
2025-04-11 20:41:05 +0700

    sua danh muc

--- backend/library/src/main/java/com/library_web/library/Respository/CategoryRepo.java
+++ backend/library/src/main/java/com/library_web/library/Respository/CategoryRepo.java
@@ -1,10 +1,14 @@
 package com.library_web.library.Respository;
 
 import com.library_web.library.Model.Category;
+
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CategoryRepo extends MongoRepository<Category, String> {
   boolean existsByTenTheLoaiChaAndTenTheLoaiCon(String tenTheLoaiCha, String tenTheLoaiCon);
+  Optional<Category> findByTenTheLoaiCon(String tenTheLoaiCon);
 }
